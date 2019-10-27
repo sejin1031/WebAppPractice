7f95a72016256b89768bb72a759e970b1a7c594a
sejin.eom <sejin1031@naver.com>
2019-10-27 16:20:00 +0900

    add lab05

--- lab05/music.php
+++ lab05/music.php
@@ -12,65 +12,108 @@
 		<h1>My Music Page</h1>
 		
 		<!-- Ex 1: Number of Songs (Variables) -->
+		<?php $songs = 5678; $hours = 100; ?>
 		<p>
 			I love music.
-			I have 1234 total songs,
-			which is over 123 hours of music!
+			I have <?php echo $songs ;?> total songs,
+			which is over <?php echo $hours ;?> hours of music!
 		</p>
 
 		<!-- Ex 2: Top Music News (Loops) -->
+		<div class="section">
+			<h2>Billboard News</h2>
+			<ol>
+				<?php for($i = 11;$i >=7; $i--) { ?>
+					<li><a href="https://www.billboard.com/archive/article/2019<?= $i ?>"> 2019-<?= $i ?> </a></li>
+				<?php } ?>
+			</ol>
+		</div>
 		<!-- Ex 3: Query Variable -->
 		<div class="section">
 			<h2>Billboard News</h2>
-		
+			<?php $newspages = 5 ?>
 			<ol>
-			    <li><a href="https://www.billboard.com/archive/article/201910">2019-11</a></li>
-				<li><a href="https://www.billboard.com/archive/article/201910">2019-10</a></li>
-				<li><a href="https://www.billboard.com/archive/article/201909">2019-09</a></li>
-				<li><a href="https://www.billboard.com/archive/article/201908">2019-08</a></li>
-				<li><a href="https://www.billboard.com/archive/article/201907">2019-07</a></li>
+				<?php for($i = 11;$i >11-$newspages; $i--) { ?>
+					<li><a href="https://www.billboard.com/archive/article/2019<?= $i ?>"> 2019-<?= $i ?> </a></li>
+				<?php } ?>
 			</ol>
 		</div>
 
 		<!-- Ex 4: Favorite Artists (Arrays) -->
+		<?php $singers = array("Gun N's Roses","Green Day","Blink182") ?>
+		<div class="section">
+			<h2>My favorite Artists</h2>
+				<ol>
+			<?php for($i = 0; $i < count($singers);$i++){ ?>
+					<li><?= $singers[$i] ?></li>
+			<?php } ?>
+				</ol>
+		</div>
 		<!-- Ex 5: Favorite Artists from a File (Files) -->
 		<div class="section">
 			<h2>My Favorite Artists</h2>
-		
+			
+			<?php $lines = file("favorite.txt") ?>
+
 			<ol>
-				<li>Guns N' Roses</li>
-				<li>Green Day</li>
-				<li>Blink182</li>
+				<?php foreach ($lines as $line){ ?>
+					<li><a href="http://en.wikipedia.org/wiki/<?= $line?>"><?= $line ?></a></li>
 			</ol>
+				<?php } ?>
 		</div>
 		
 		<!-- Ex 6: Music (Multiple Files) -->
+		<div class="section">
+			<h2>My Music and Playlists</h2>
+			<?php $path = "./lab5/musicPHP/songs/";
+			$songs = scandir($path);	?>
+			<ul>
+				<?php foreach($songs as $song){ 
+					if($song == '.' || $song == '..') continue ;
+					else { ?>
+					<li class="mp3item"><?= $song ?></li>
+					<?php } } ?>
+
+			</ul>
+		</div>
 		<!-- Ex 7: MP3 Formatting -->
 		<div class="section">
 			<h2>My Music and Playlists</h2>
-
-			<ul id="musiclist">
-				<li class="mp3item">
-					<a href="lab5/musicPHP/songs/paradise-city.mp3">paradise-city.mp3</a>
-				</li>
-				
-				<li class="mp3item">
-					<a href="lab5/musicPHP/songs/basket-case.mp3">basket-case.mp3</a>
-				</li>
-
-				<li class="mp3item">
-					<a href="lab5/musicPHP/songs/all-the-small-things.mp3">all-the-small-things.mp3</a>
-				</li>
+			
+			<?php $path = "./lab5/musicPHP/songs/";
+			$songs = scandir($path);	?>
+			<ul>
+				<?php foreach($songs as $song){
+					$fileinfo = pathinfo($song);
+					$ext = $fileinfo['extension'];
+					if($song == '.' || $song == '..' || $ext != 'mp3') continue ;
+					else { ?>
+					<li class="mp3item">
+						<a href="lab5/musicPHP/songs/<?= $song ?>" download><?= $song ?> </a>
+						(<?= floor(filesize("./lab5/musicPHP/songs/$song")/1024) ?>KB)
+					</li>
+					<?php } } ?>			
 
 				<!-- Exercise 8: Playlists (Files) -->
-				<li class="playlistitem">326-13f-mix.m3u:
-					<ul>
-						<li>Basket Case.mp3</li>
-						<li>All the Small Things.mp3</li>
-						<li>Just the Way You Are.mp3</li>
-						<li>Pradise City.mp3</li>
-						<li>Dreams.mp3</li>
-					</ul>
+				<?php foreach($songs as $song){
+					$fileinfo = pathinfo($song);
+					$ext = $fileinfo['extension'];
+					if($ext != 'm3u'){
+						continue;
+					}
+					else{ ?>
+					<li class="playlistitem"> <?= $song ?>:
+						<ul>
+						<?php $lines = file("./lab5/musicPHP/songs/$song");
+						foreach($lines as $line){ 
+							if(strpos($line, '#') !== false) { continue; }
+							else {?>
+						<li> <?= $line ?> </li>
+							<?php } ?>
+						<?php } ?>
+						</ul>
+						</li>
+						<?php } } ?>
 			</ul>
 		</div>
 
